890d0c2103745d59bf85d1143526744132b3a4df
firdavsxon <akilovfs@gmail.com>
2020-07-22 15:35:42 -0700

    update

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import cv2
 
 detect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-imp_img = cv2.VideoCapture('elon.jpg')
+imp_img = cv2.VideoCapture(0)
 
 res, img = imp_img.read()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -13,7 +13,7 @@ faces = detect.detectMultiScale(gray, 1.3, 5)
 for (x,y,w, h) in faces:
 	cv2.rectangle(img, (x,y), (x+w, y+h), (0, 200, 0, 0), 2)
 
-cv2.imshow("Elon image", img)
+cv2.imshow("My image", img)
 cv2.waitKey(0)
 imp_img.release()
 cv2.destroyAllWindows()
